a046f623f044ecba264509df94eff3b02caeed91
alklimashonak <alklimashonak@gmail.com>
2022-04-14 20:24:26 +0200

    add basic tests for team apis

---
+++ tests/test_teams.py
@@ -0,0 +1,40 @@
+def test_read_all_teams_api_valid(create_two_teams, client):
+    response = client.get('/teams')
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+    assert response.json() == [
+        {
+            'id': 1,
+            'name': 'team 1',
+        },
+        {
+            'id': 2,
+            'name': 'team 2',
+        },
+    ]
+
+
+def test_read_team_api_valid(create_two_teams, client):
+    response = client.get('/teams/2')
+    assert response.status_code == 200
+    assert response.json() == {
+        'id': 2,
+        'name': 'team 2',
+        'drivers': [],
+        'owner': {
+            'id': 1,
+            'username': 'user',
+            'email': None
+        }
+    }
+
+
+def test_create_team_api(create_two_teams, client):
+    response = client.get('/teams')
+    assert len(response.json()) == 2
+    client.post('/teams?user_id=1',
+                json={
+                    'name': 'test team'
+                })
+    response = client.get('/teams')
+    assert len(response.json()) == 3
